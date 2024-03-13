26fe6949724c8efcb8340fdb6a2a7c0e54fe36df
aflijihen <99421977+aflijihen@users.noreply.github.com>
2024-03-13 23:56:42 +0100

    Update rag_langchain.py

--- rag_langchain.py
+++ rag_langchain.py
@@ -130,8 +130,7 @@ def main():
         response = result["answer"]
         memory.chat_memory.add_ai_message(response)
         print("AI:", response)
-          # Obtenir et afficher la recommandation
-    #     get_recommendation(response, vectorstore, embedding)
+       
      # Stop MQTT client loop when exiting the while loop
         client.loop_stop()
 
@@ -141,109 +140,4 @@ def main():
 if __name__ == "__main__":
     main()
 
-# import os
-# import json
-# import time 
-# import logging
-# import argparse
-# import requests
-# import paho.mqtt.client as mqtt
-# from weather_api import get_weather_data
 
-# from langchain.chains import ConversationalRetrievalChain
-# from langchain.chat_models.openai import ChatOpenAI
-# from langchain.document_loaders import DirectoryLoader, Docx2txtLoader
-# from langchain.embeddings import SentenceTransformerEmbeddings
-# from langchain.memory import ConversationBufferMemory
-# from langchain.text_splitter import RecursiveCharacterTextSplitter
-# from langchain.vectorstores.faiss import FAISS
-    
-
-# class MQTTClient:
-#     def __init__(self, broker_address, topic):  #Initialise un client MQTT avec l'adresse du courtier et le sujet spécifiés.
-#         self.broker_address = broker_address
-#         self.topic = topic
-#         self.received_data = None
-#         self.client = mqtt.Client()
-#         self.client.on_message = self.on_message
-
-#     def on_message(self, client, userdata, msg):
-#         self.received_data = json.loads(msg.payload.decode())
-#         print("Received data:", self.received_data)
-
-#     def connect(self):
-#         self.client.connect(self.broker_address)
-#         self.client.subscribe(self.topic)
-#         self.client.loop_start()
-
-#     def disconnect(self):
-#         self.client.loop_stop()
-
-# class ChatSystem:
-#     def __init__(self, embedding_model="all-mpnet-base-v2", api_key="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", temperature=0.6):
-#         self.embedding = SentenceTransformerEmbeddings(model_name=embedding_model)
-#         self.llm = ChatOpenAI(api_key=api_key, temperature=temperature)
-#         self.memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-#         self.memory.load_memory_variables({})
-#         self.qa_chain = None
-
-#     def build_qa_chain(self, docs_dir="./handbook/", persist_dir="./handbook_faiss"):
-#         if os.path.exists(persist_dir): 
-#             print(f"Loading FAISS index from {persist_dir}")
-#             vectorstore = FAISS.load_local(persist_dir, self.embedding, allow_dangerous_deserialization=True)
-#             print("done.")
-#         else:
-#             print(f"Building FAISS index from documents in {docs_dir}")
-#             loader = DirectoryLoader(docs_dir,
-#                 loader_cls=Docx2txtLoader,
-#                 recursive=True,
-#                 silent_errors=True,
-#                 show_progress=True,
-#                 glob="**/*.docx"  # which files get loaded
-#             )
-#             docs = loader.load()
-#             text_splitter = RecursiveCharacterTextSplitter(
-#                 chunk_size=500,
-#                 chunk_overlap=75
-#             )
-#             frags = text_splitter.split_documents(docs)
-#             print(f"Populating vector store with {len(docs)} docs in {len(frags)} fragments")
-#             vectorstore = FAISS.from_documents(frags, self.embedding)
-#             print(f"Persisting vector store to: {persist_dir}")
-#             vectorstore.save_local(persist_dir)
-#             print(f"Saved FAISS index to {persist_dir}")
-
-#         self.qa_chain = ConversationalRetrievalChain.from_llm(
-#             llm=self.llm,
-#             memory=self.memory,
-#             retriever=vectorstore.as_retriever()
-#         )
-
-  
-#     def chat(self):
-    
-#         while True:
-#             user_input = input("Enter your input: ")
-#             if user_input == "exit":
-#                     break
-#             self.memory.chat_memory.add_user_message(user_input)
-#             result = self.qa_chain({"question": user_input})
-#             response = result["answer"]
-#             self.memory.chat_memory.add_ai_message(response)
-#             print("AI:", response)
-
-# def main():
-#     broker_address = "mqtt.eclipseprojects.io"
-#     topic = "Spirulina_Edge"
-#     mqtt_client = MQTTClient(broker_address, topic)
-#     mqtt_client.connect()
-
-#     chat_system = ChatSystem()
-#     chat_system.build_qa_chain()
-#     chat_system.chat()
-
-#     mqtt_client.disconnect()
-   
-
-# if __name__ == "__main__":
-#     main()
